f2009e5167fc1ace25e06a09a2b821f900755383
Alex Kang <alexkang@alexs-air-3.lan>
2023-04-15 23:07:21 -0600

    deployment code working

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/sorting-visualized-vue/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/sorting-visualized-vue/'
-    : '/'
+  }
 })
